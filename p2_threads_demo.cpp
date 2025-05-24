204131ea4d33d83716a591d387f8f9ef45435be1
713068_Ashim <ashimaakash1997@gmail.com>
2025-05-24 23:37:50 +0530

    demonstrated threads using lambda

--- p2_threads_demo.cpp
+++ p2_threads_demo.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <thread>
+// #include <iostream>
+// #include <thread>
 
-void test(int x){
+// void test(int x){
 
-    std::cout<<"Hello from test function "<<std::endl;
-    std::cout<<"Argument passed in "<<x<< std::endl;
+//     std::cout<<"Hello from test function "<<std::endl;
+//     std::cout<<"Argument passed in "<<x<< std::endl;
 
-}
+// }
 
-int main(){
+// int main(){
 
-    std::cout<<"Hello from main thread... "<<std::endl;
+//     std::cout<<"Hello from main thread... "<<std::endl;
 
-    std::thread thr1(&test, 25);
-    std::thread thr2(&test, 26);
-    std::thread thr3(&test, 27);
+//     std::thread thr1(&test, 25);
+//     std::thread thr2(&test, 26);
+//     std::thread thr3(&test, 27);
 
-    thr1.joinable();
-    thr1.join();
-    thr1.joinable();
+//     thr1.joinable();
+//     thr1.join();
+//     thr1.joinable();
 
-    thr2.join();
-    thr3.join();
-    return 0;
-}
+//     thr2.join();
+//     thr3.join();
+//     return 0;
+// }
