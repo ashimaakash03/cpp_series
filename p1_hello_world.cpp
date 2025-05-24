41edafb4e24440164ed436eb73c70140caa422f3
713068_Ashim <ashimaakash1997@gmail.com>
2025-05-24 22:54:11 +0530

    demonstrated threads in cpp

--- p1_hello_world.cpp
+++ p1_hello_world.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+// #include <iostream>
 
-int main()
-{
-    std::cout << "Enter a number " << std::endl;
-    int x;
-    std::cin >> x;
-    std::cout << "Hello World! " << x << std::endl;
-    return 0;
-}
+// int main()
+// {
+//     std::cout << "Enter a number " << std::endl;
+//     int x;
+//     std::cin >> x;
+//     std::cout << "Hello World! " << x << std::endl;
+//     return 0;
+// }
---
+++ p2_threads_demo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+
+void test(int x){
+
+    std::cout<<"Hello from test function "<<std::endl;
+    std::cout<<"Argument passed in "<<x<< std::endl;
+
+}
+
+int main(){
+
+    std::cout<<"Hello from main thread... "<<std::endl;
+
+    std::thread thr1(&test, 25);
+    std::thread thr2(&test, 26);
+    std::thread thr3(&test, 27);
+
+    thr1.joinable();
+    thr1.join();
+    thr1.joinable();
+
+    thr2.join();
+    thr3.join();
+    return 0;
+}
