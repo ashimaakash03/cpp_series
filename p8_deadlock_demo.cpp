e1fef4ab2ccca24b84682f6ef4189395d80a5276
713068_Ashim <ashimaakash1997@gmail.com>
2025-05-25 19:55:59 +0530

    implemented lock_guard

--- p8_deadlock_demo.cpp
+++ p8_deadlock_demo.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <thread>
-#include <vector>
-#include <mutex>
+// #include <iostream>
+// #include <thread>
+// #include <vector>
+// #include <mutex>
 
 
-std::mutex a_mutex;
+// std::mutex a_mutex;
 
 
-static int shared_value = 25;
-void increase_shared_value()
-{
-    a_mutex.lock();
-        for(int i=1; i<=1000; i++) shared_value+=i;
-    //a_mutex.unlock();
-    //here, on forgetting to call the unlock method, we observe a "deadlock" situation wherein none of the other threads are able to execute, because the first thread has locked the mutex and exit the scope 
-    //The code will freeze
-}
+// static int shared_value = 25;
+// void increase_shared_value()
+// {
+//     a_mutex.lock();
+//         for(int i=1; i<=1000; i++) shared_value+=i;
+//     //a_mutex.unlock();
+//     //here, on forgetting to call the unlock method, we observe a "deadlock" situation wherein none of the other threads are able to execute, because the first thread has locked the mutex and exit the scope 
+//     //The code will freeze
+// }
 
-int main()
-{
+// int main()
+// {
 
-    std::vector<std::thread> threads;
-    for (int i = 0; i <= 100; i++)
-        threads.push_back(std::thread(increase_shared_value));
-    for (int i = 0; i <= 100; i++)
-        threads[i].join();
+//     std::vector<std::thread> threads;
+//     for (int i = 0; i <= 100; i++)
+//         threads.push_back(std::thread(increase_shared_value));
+//     for (int i = 0; i <= 100; i++)
+//         threads[i].join();
 
-    std::cout << "Final value of shared value is: " << shared_value << std::endl;
-    return 0;
-}
+//     std::cout << "Final value of shared value is: " << shared_value << std::endl;
+//     return 0;
+// }
---
+++ p9_lock_guard_demo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+
+std::mutex a_mutex;
+
+
+static int shared_value = 25;
+void increase_shared_value()
+{
+    std::lock_guard<std::mutex> guard(a_mutex);
+
+    for(int i=1; i<=1000; i++) shared_value+=i;
+    
+}
+
+int main()
+{
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i <= 100; i++)
+        threads.push_back(std::thread(increase_shared_value));
+    for (int i = 0; i <= 100; i++)
+        threads[i].join();
+
+    std::cout << "Final value of shared value is: " << shared_value << std::endl;
+    return 0;
+}
