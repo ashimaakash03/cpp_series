9a4cadb021b0222c35567a9a7a00345796bcf641
713068_Ashim <ashimaakash1997@gmail.com>
2025-05-25 11:30:02 +0530

    implemented jthread

---
+++ p5_inroduction_to_jthreads.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+int main()
+{
+
+    auto lambda = [](int x)
+    {
+        std::cout << "Hello from test function " << std::this_thread::get_id() << std::endl;
+        std::cout << "Argument passed in " << x << std::endl;
+    };
+
+    std::vector<std::jthread> threads;
+    for (int i = 1; i <= 10; i++)
+        threads.push_back(std::jthread(lambda, i));
+
+    std::cout << "Hello from main thread... " << std::endl;
+
+    return 0;
+}
