2de495a4656cb793146c603e01550067d991b8f1
713068_Ashim <ashimaakash1997@gmail.com>
2025-05-25 14:05:36 +0530

    demonstrated deadlock situation

--- p7_introduction_to_mutex.cpp
+++ p7_introduction_to_mutex.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <thread>
-#include <vector>
-#include <mutex>
+// #include <iostream>
+// #include <thread>
+// #include <vector>
+// #include <mutex>
 
 
-std::mutex a_mutex;
+// std::mutex a_mutex;
 
 
-static int shared_value = 25;
-void increase_shared_value()
-{
-    a_mutex.lock();
-        for(int i=1; i<=1000; i++) shared_value+=i;
-    a_mutex.unlock();
-}
+// static int shared_value = 25;
+// void increase_shared_value()
+// {
+//     a_mutex.lock();
+//         for(int i=1; i<=1000; i++) shared_value+=i;
+//     a_mutex.unlock();
+// }
 
-int main()
-{
+// int main()
+// {
 
-    std::vector<std::thread> threads;
-    for (int i = 0; i <= 100; i++)
-        threads.push_back(std::thread(increase_shared_value));
-    for (int i = 0; i <= 100; i++)
-        threads[i].join();
+//     std::vector<std::thread> threads;
+//     for (int i = 0; i <= 100; i++)
+//         threads.push_back(std::thread(increase_shared_value));
+//     for (int i = 0; i <= 100; i++)
+//         threads[i].join();
 
-    std::cout << "Final value of shared value is: " << shared_value << std::endl;
-    return 0;
-}
+//     std::cout << "Final value of shared value is: " << shared_value << std::endl;
+//     return 0;
+// }
