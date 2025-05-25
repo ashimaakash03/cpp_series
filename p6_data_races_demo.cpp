a6bbe81371815cb8513ab8e58695f8aed4be3a7e
713068_Ashim <ashimaakash1997@gmail.com>
2025-05-25 12:14:57 +0530

    implemented a scenario that causes data race

---
+++ p6_data_races_demo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+static int shared_value = 25;
+void increase_shared_value()
+{
+    for(int i=1; i<=1000; i++){
+        shared_value+=i;
+    }
+}
+
+int main()
+{
+
+    std::vector<std::thread> threads;
+    for (int i = 1; i <= 100; i++)
+        threads.push_back(std::thread(increase_shared_value));
+    for (int i = 1; i <= 100; i++)
+        threads[i].join();
+
+    std::cout << "Final value of shared value is: " << shared_value << std::endl;
+    return 0;
+}
